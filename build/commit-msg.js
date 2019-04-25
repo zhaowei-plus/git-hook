72f341851f66245b57f598ef0da9c494f2dfa331
wb-zhaowei@cai-inc.com <1215058679@qq.com>
2019-04-25 16:18:40 +0800

    xxx

--- build/commit-msg.js
+++ build/commit-msg.js
@@ -1,5 +1,18 @@
-/**
- * commit-msg 钩子接收一个参数：存有当前提交信息的临时文件的路径
- *  
- */
-console.log('commit-msg:', process.argv);
+#!/use/bin/env node
+
+const fs = require('fs');
+
+console.log(`
+  commit-msg 钩子接收一个参数，此参数即上文提到的，存有当前提交信息的临时文件的路径。 
+  如果该钩子脚本以非零值退出，Git 将放弃提交，因此，可以用来在提交通过前验证项目状态或提交信息。
+`);
+
+const filePath = process.argv[1];
+console.log(`当前提交文件：${filePath}`);
+
+if (filePath.indexOf('package.json') > -1) {
+  console.log('package.json 被限制提交');
+  return 1;
+}
+
+return 0
